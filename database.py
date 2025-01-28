d90c697744efa0ee52fa58761fd4f7acce36b1b2
Sophia <ms.shb2004@mail.ru>
2025-01-28 21:54:33 +0300

    Refactor: Add an error handler

--- database.py
+++ database.py
@@ -1,18 +1,23 @@
 import aiosqlite
+import logging
 
 
 async def init_db():
-    async with aiosqlite.connect('action.db') as db:
-        await db.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            userId INTEGER PRIMARY KEY AUTOINCREMENT,
-            act TEXT,
-            fullname TEXT,
-            data TEXT,
-            persons TEXT,
-            gender TEXT,
-            room TEXT,
-            pastRooms TEXT
-        )""")
-        await db.commit()
+    logging.info("Initializing the database.")
+    try:
+        async with aiosqlite.connect('action.db') as db:
+            await db.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                userId INTEGER PRIMARY KEY AUTOINCREMENT,
+                act TEXT,
+                fullname TEXT,
+                data TEXT,
+                persons TEXT,
+                gender TEXT,
+                room TEXT,
+                pastRooms TEXT
+            )""")
+            await db.commit()
 
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
