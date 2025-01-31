b83e55a006be91f25912180eb181671d68890edc
Sophia <ms.shb2004@mail.ru>
2025-01-31 16:37:32 +0300

    refactor: Rename variables to avoid shadowing

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 
 
 base_path = os.path.dirname(os.path.abspath(__file__))
-settings_file = os.path.join(base_path, 'tokens.txt')
+settings_file_path = os.path.join(base_path, 'tokens.txt')
 
 
-def read_config(settings_file):
-    config = {}
-    with open(settings_file, 'r') as file:
+def read_config(file_path):
+    config_dict = {}
+    with open(file_path, 'r') as file:
         for line in file:
             key, value = line.strip().split('=')
-            config[key] = value
-    return config
+            config_dict[key] = value
+    return config_dict
 
 
-config = read_config(settings_file)
+config = read_config(settings_file_path)
 TOKEN_VK = config['TOKEN_VK']
 TOKEN_TG = config['TOKEN_TG']
 CHANNEL_ID = config['CHANNEL_ID']
