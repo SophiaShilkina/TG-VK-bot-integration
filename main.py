166f4cb20b02b98c5c65075818e60082a15f2ecf
Sophia <ms.shb2004@mail.ru>
2025-02-02 17:08:00 +0300

    fix: Create an asynchronous function to terminate the program

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ async def main():
     await asyncio.gather(main_bot(), main_loop())
 
 
+async def shutdown():
+    logging.info("Shutting down...")
+    await asyncio.sleep(5)
+
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        asyncio.sleep(5)
+        asyncio.run(shutdown())
         pass
