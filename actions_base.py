d8d23d6ff20922d7666a7e711f58cd9d0485dbde
Sophia <ms.shb2004@mail.ru>
2025-02-22 15:46:49 +0300

    feat: Add a condition for the genders and for the rooms

--- actions_base.py
+++ actions_base.py
@@ -1,9 +1,10 @@
 from base import async_session
 from models import UsersBase
-from methods import get_fullname, send_message
+from methods import get_fullname, send_message, upload_photo_to_vk
 from responses_logic import message_handler
 from sqlalchemy import select, update
 import re
+from config import get_cached_photo_url, cache_photo_url
 
 
 # Проверяем пользователя в базе данных и возвращаем его шаг
@@ -60,7 +61,7 @@ async def message_validation(user_id, user_act, message_incoming) -> str:
 commands_tpl = ('начать', '📆 изменить даты', '👔 изменить число персон', '👫 изменить соотношение',
                 '🏡 изменить комнаты', 'все верно', 'прочитал(а) и принимаю правила', 'позвать оператора')
 acts_tpl = ('start', 'dates', 'dates_mistake', 'persons', 'persons_mistake', 'genders', 'genders_mistake',
-            'rooms')
+            'rooms', 'all_right')
 
 
 # Реализация логики шагов пользователя через словарь
@@ -68,14 +69,15 @@ def changed_act(user_act) -> str:
     changed_acts_dict = {'start': 'dates',
                          'dates': 'persons',
                          'persons': 'genders',
-                         'genders': 'rooms'}
+                         'genders': 'rooms',
+                         'rooms': 'all_right'}
     user_act = changed_acts_dict.get(user_act)
     return user_act
 
 
 async def user_act_handler(user_id, user_act, message_incoming, acts_tuple=acts_tpl):
     async with (async_session() as session):
-        if message_incoming == 'начать' or message_incoming == 'start':
+        if message_incoming in ('начать', 'start'):
             messages_outgoing = message_handler(user_act)
             if type(messages_outgoing) is tuple:
                 for message_outgoing in messages_outgoing:
@@ -89,12 +91,29 @@ async def user_act_handler(user_id, user_act, message_incoming, acts_tuple=acts_
             return
 
         if user_act in acts_tuple:
-            if user_act in ('dates', 'persons', 'genders', 'rooms'):
+            if user_act in ('dates', 'persons', 'genders'):
                 user_act = await message_validation(user_id, user_act, message_incoming)
-                if user_act in ('dates', 'persons', 'genders', 'rooms'):
+                if user_act in ('dates', 'persons'):
                     await send_message(user_id, message_handler(user_act))
+
                     column_value = {user_act: message_incoming}
                     stmt = update(UsersBase).where(UsersBase.userId == user_id).values(**column_value,
+                                                                                       act=changed_act(user_act))
+                    await session.execute(stmt)
+                    await session.commit()
+                    return
+
+                elif user_act == 'genders':
+                    photo_url = await get_cached_photo_url()
+
+                    if not photo_url:
+                        print('----------111', photo_url)
+                        photo_url = await upload_photo_to_vk()
+                        print('--------------------112', photo_url)
+                        await cache_photo_url(photo_url)
+
+                    await send_message(user_id, message_handler(user_act), photo_url)
+                    stmt = update(UsersBase).where(UsersBase.userId == user_id).values(genders=message_incoming,
                                                                                        act=changed_act(user_act))
                     await session.execute(stmt)
                     await session.commit()
@@ -106,3 +125,24 @@ async def user_act_handler(user_id, user_act, message_incoming, acts_tuple=acts_
                     await session.execute(stmt)
                     await session.commit()
                     return
+
+            if user_act == 'rooms':
+                stmt = update(UsersBase).where(UsersBase.userId == user_id).values(rooms=message_incoming,
+                                                                                   pastRooms=message_incoming,
+                                                                                   act=changed_act(user_act))
+                await session.execute(stmt)
+                await session.commit()
+
+                stmt = select(UsersBase.dates,
+                              UsersBase.persons,
+                              UsersBase.genders,
+                              UsersBase.rooms).where(UsersBase.userId == user_id)
+                result = await session.execute(stmt)
+
+                user_data = result.fetchall()[0]
+
+                dates, persons, genders, rooms = user_data[0], user_data[1], user_data[2], user_data[3],
+                await send_message(user_id, message_handler(user_act).format(dates, persons, genders, rooms))
+
+
+                
