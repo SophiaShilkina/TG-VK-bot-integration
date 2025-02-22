00d782bdb376601cc1893bad7d40a2f51e0f994b
Sophia <ms.shb2004@mail.ru>
2025-02-22 15:47:30 +0300

    feat: Add asynchronous functions for opening the cache file

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+import aiofiles
 
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 settings_file_path = os.path.join(base_path, 'docs/tokens.txt')
+CACHE_FILE = 'docs/photo_cache.txt'
 
 
 def read_config(file_path):
@@ -22,3 +24,15 @@ CONFIRMATION = config['CONFIRMATION']
 API_V = config['API_V']
 SECRET = config['SECRET']
 print(TOKEN_VK, TOKEN_TG, CHANNEL_ID, CONFIRMATION, API_V, SECRET, sep='\n')
+
+
+async def cache_photo_url(photo_url):
+    async with aiofiles.open(CACHE_FILE, 'w') as file:
+        await file.write(photo_url)
+
+
+async def get_cached_photo_url():
+    if os.path.exists(CACHE_FILE):
+        async with aiofiles.open(CACHE_FILE, 'r') as file:
+            return await file.read()
+    return None
