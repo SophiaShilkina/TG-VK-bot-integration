550cfbacc6fe889d04fe8fe5416716d26db807b6
Sophia <ms.shb2004@mail.ru>
2025-02-22 15:48:30 +0300

    feat: Add the function of uploading images to the VK server

--- methods.py
+++ methods.py
@@ -1,9 +1,11 @@
 import httpx
 import random
 from config import TOKEN_VK, API_V
+import aiofiles
+import os
 
 
-async def send_message(user_id, message):
+async def send_message(user_id, message, attachment=None, keyboard=None):
     url = 'https://api.vk.com/method/messages.send'
     params = {
         'access_token': TOKEN_VK,
@@ -12,6 +14,11 @@ async def send_message(user_id, message):
         'message': message,
         'random_id': random.randint(1, 1000000)
     }
+    if attachment:
+        params['attachment'] = attachment
+    if keyboard:
+        params['keyboard'] = keyboard
+
     async with httpx.AsyncClient() as client:
         await client.get(url, params=params)
 
@@ -23,6 +30,7 @@ async def get_fullname(user_id):
         'access_token': TOKEN_VK,
         'v': API_V
     }
+
     async with httpx.AsyncClient() as client:
         response = await client.get(url, params=params)
 
@@ -36,3 +44,58 @@ async def get_fullname(user_id):
                 fullname = first_name + ' ' + last_name
 
                 return fullname
+
+
+async def upload_photo_to_vk():
+    try:
+        url = 'https://api.vk.com/method/photos.getMessagesUploadServer'
+        params = {
+            'access_token': TOKEN_VK,
+            'v': API_V,
+            'group_id': '226206756'
+        }
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+            url_info = response.json()['response']
+            upload_url = url_info.get('upload_url', 'Не указано')
+            print('----------------------115', upload_url)
+
+            async with aiofiles.open('docs/prices.jpg', 'rb') as photo:
+                print('----------------------120', photo)
+                file_data = await photo.read()
+                upload_response = await client.post(upload_url, files={'photo': file_data})
+                print('----------------------117', upload_response)
+                upload_data = upload_response.json()
+                print('----------------------118', upload_data)
+
+                url = await save_photo(upload_data['photo'], upload_data['server'], upload_data['hash'])
+                print('----------------------113', url)
+
+                return url
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return None
+
+
+async def save_photo(photo, server, hash):
+    url = 'https://api.vk.com/method/photos.saveMessagesPhoto'
+    params = {
+        'access_token': TOKEN_VK,
+        'v': API_V,
+        'photo': photo,
+        'server': server,
+        'hash': hash
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, params=params)
+        response_data = response.json()
+        print('----------------------114', response_data)
+        if 'response' not in response_data:
+            raise ValueError("Ошибка сохранения фото: " + str(response_data))
+
+        photo_data = response_data['response'][0]
+        photo_url = f"photo{photo_data['owner_id']}_{photo_data['id']}"
+        return photo_url
