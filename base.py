adbb232476cee442eda27d02908f83163e8e3034
Sophia <ms.shb2004@mail.ru>
2025-02-23 14:19:27 +0300

    move: Change the position of the db

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from models import Base
 
 
-DATABASE_URL = "sqlite+aiosqlite:///./action.db"
+DATABASE_URL = "sqlite+aiosqlite:///./docs/action.db"
 
 engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
