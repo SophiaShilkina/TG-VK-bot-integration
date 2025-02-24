5b1da3ffc083e8df6cfcffce5959df03ffa0ea73
Sophia <ms.shb2004@mail.ru>
2025-02-24 16:43:19 +0300

    feat: Add callback processing logic

--- actions_base.py
+++ actions_base.py
@@ -5,6 +5,7 @@ from responses_logic import message_handler
 from sqlalchemy import select, update
 import re
 from config import get_cached_photo_url, cache_photo_url
+from keyboards import *
 
 
 # Проверяем пользователя в базе данных и возвращаем его шаг
@@ -46,10 +47,10 @@ async def message_validation(user_id, user_act, message_incoming) -> str:
             if not re.match(r'^\s*\d{1,3}\s*\.*\s*$', message_incoming):
                 user_act = 'persons_mistake'
 
-        if user_act == 'gender':
+        if user_act == 'genders':
             if not re.match(r'^\s*\d{1,3}\s*[МЖмужчинаещ]*\s*[\\/]\s*\d{1,3}\s*[МЖмужчинаещ]*\s*\.*\s*$',
                             message_incoming):
-                user_act = 'gender_mistake'
+                user_act = 'genders_mistake'
 
         stmt = update(UsersBase).where(UsersBase.userId == user_id).values(act=user_act)
         await session.execute(stmt)
@@ -60,8 +61,6 @@ async def message_validation(user_id, user_act, message_incoming) -> str:
 
 commands_tpl = ('начать', '📆 изменить даты', '👔 изменить число персон', '👫 изменить соотношение',
                 '🏡 изменить комнаты', 'все верно', 'прочитал(а) и принимаю правила', 'позвать оператора')
-acts_tpl = ('start', 'dates', 'dates_mistake', 'persons', 'persons_mistake', 'genders', 'genders_mistake',
-            'rooms', 'all_right')
 
 
 # Реализация логики шагов пользователя через словарь
@@ -75,9 +74,24 @@ def changed_act(user_act) -> str:
     return user_act
 
 
-async def user_act_handler(user_id, user_act, message_incoming, acts_tuple=acts_tpl):
+async def change_act_after_callback(user_id, user_act) -> None:
+    async with (async_session() as session):
+        stmt = update(UsersBase).where(UsersBase.userId == user_id).values(act=user_act)
+        await session.execute(stmt)
+        await session.commit()
+
+        stmt = select(UsersBase.act).where(UsersBase.userId == user_id)
+        result = await session.execute(stmt)
+
+        act = result.scalar_one_or_none()
+
+        return act
+
+
+async def user_act_handler(user_id, user_act, message_incoming=None):
     async with (async_session() as session):
         if message_incoming in ('начать', 'start'):
+            print(f"Обработка пользователя: {user_id}, действие: {user_act}, входящее сообщение: {message_incoming}")
             messages_outgoing = message_handler(user_act)
             if type(messages_outgoing) is tuple:
                 for message_outgoing in messages_outgoing:
@@ -90,59 +104,62 @@ async def user_act_handler(user_id, user_act, message_incoming, acts_tuple=acts_
             await session.commit()
             return
 
-        if user_act in acts_tuple:
-            if user_act in ('dates', 'persons', 'genders'):
-                user_act = await message_validation(user_id, user_act, message_incoming)
-                if user_act in ('dates', 'persons'):
-                    await send_message(user_id, message_handler(user_act))
+        if user_act in ('dates', 'persons', 'genders'):
+            user_act = await message_validation(user_id, user_act, message_incoming)
+            if user_act in ('dates', 'persons'):
+                await send_message(user_id, message_handler(user_act))
 
-                    column_value = {user_act: message_incoming}
-                    stmt = update(UsersBase).where(UsersBase.userId == user_id).values(**column_value,
-                                                                                       act=changed_act(user_act))
-                    await session.execute(stmt)
-                    await session.commit()
-                    return
-
-                elif user_act == 'genders':
-                    photo_url = await get_cached_photo_url()
-
-                    if not photo_url:
-                        print('----------111', photo_url)
-                        photo_url = await upload_photo_to_vk()
-                        print('--------------------112', photo_url)
-                        await cache_photo_url(photo_url)
-
-                    await send_message(user_id, message_handler(user_act), photo_url)
-                    stmt = update(UsersBase).where(UsersBase.userId == user_id).values(genders=message_incoming,
-                                                                                       act=changed_act(user_act))
-                    await session.execute(stmt)
-                    await session.commit()
-                    return
-
-                else:
-                    await send_message(user_id, message_handler(user_act))
-                    stmt = update(UsersBase).where(UsersBase.userId == user_id).values(act=user_act.split('_')[0])
-                    await session.execute(stmt)
-                    await session.commit()
-                    return
-
-            if user_act == 'rooms':
-                stmt = update(UsersBase).where(UsersBase.userId == user_id).values(rooms=message_incoming,
-                                                                                   pastRooms=message_incoming,
+                column_value = {user_act: message_incoming}
+                stmt = update(UsersBase).where(UsersBase.userId == user_id).values(**column_value,
                                                                                    act=changed_act(user_act))
                 await session.execute(stmt)
                 await session.commit()
+                return
 
-                stmt = select(UsersBase.dates,
-                              UsersBase.persons,
-                              UsersBase.genders,
-                              UsersBase.rooms).where(UsersBase.userId == user_id)
-                result = await session.execute(stmt)
+            elif user_act == 'genders':
+                photo_url = await get_cached_photo_url()
 
-                user_data = result.fetchall()[0]
+                if not photo_url:
+                    photo_url = await upload_photo_to_vk()
+                    await cache_photo_url(photo_url)
 
-                dates, persons, genders, rooms = user_data[0], user_data[1], user_data[2], user_data[3],
-                await send_message(user_id, message_handler(user_act).format(dates, persons, genders, rooms))
+                await send_message(user_id, message_handler(user_act), photo_url)
+                stmt = update(UsersBase).where(UsersBase.userId == user_id).values(genders=message_incoming,
+                                                                                   act=changed_act(user_act))
+                await session.execute(stmt)
+                await session.commit()
+                return
 
+            else:
+                await send_message(user_id, message_handler(user_act))
+                stmt = update(UsersBase).where(UsersBase.userId == user_id).values(act=user_act.split('_')[0])
+                await session.execute(stmt)
+                await session.commit()
+                return
 
-                
+        if user_act == 'rooms':
+            stmt = update(UsersBase).where(UsersBase.userId == user_id).values(rooms=message_incoming,
+                                                                               pastRooms=message_incoming,
+                                                                               act=changed_act(user_act))
+            await session.execute(stmt)
+            await session.commit()
+
+            stmt = select(UsersBase.dates,
+                          UsersBase.persons,
+                          UsersBase.genders,
+                          UsersBase.rooms).where(UsersBase.userId == user_id)
+            result = await session.execute(stmt)
+
+            user_data = result.fetchall()[0]
+
+            dates, persons, genders, rooms = user_data[0], user_data[1], user_data[2], user_data[3],
+            await send_message(user_id, message_handler(user_act).format(dates, persons, genders, rooms),
+                               keyboard=keyboard)
+
+        if user_act in ('dates_changed', 'persons_changed', 'genders_changed', 'rooms_changed'):
+            await send_message(user_id, message_handler(user_act))
+
+            stmt = update(UsersBase).where(UsersBase.userId == user_id).values(act=user_act.split('_')[0])
+            await session.execute(stmt)
+            await session.commit()
+            return
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request, Response
 from config import CONFIRMATION
-from actions_base import check_user_in_database_and_return_act, user_act_handler
+from actions_base import check_user_in_database_and_return_act, user_act_handler, change_act_after_callback
 from base import database_implementation
 import asyncio
 
@@ -12,20 +12,35 @@ async def callback(request: Request):
     data = await request.json()
     print(data)
 
-    if 'type' in data:
-        if data['type'] == 'confirmation':
-            return Response(content=CONFIRMATION, media_type="text/plain")
+    try:
+        if 'type' in data:
+            if data['type'] == 'confirmation':
+                return Response(content=CONFIRMATION, media_type="text/plain")
 
-        elif data['type'] == 'message_new':
-            message_info = data['object']['message']
-            user_id = message_info.get('from_id', 'Не указано')
-            message_incoming = message_info.get('text', 'Не указано').lower()
+            elif data['type'] == 'message_new':
+                message_info = data['object']['message']
+                user_id = message_info.get('from_id', 'Не указано')
+                message_incoming = message_info.get('text', 'Не указано').lower()
+                user_act = await check_user_in_database_and_return_act(user_id)
 
-            user_act = await check_user_in_database_and_return_act(user_id)
-            await user_act_handler(user_id, user_act, message_incoming)
+                asyncio.create_task(user_act_handler(user_id, user_act, message_incoming))
+                return Response(content='ok', media_type="text/plain")
 
-            return Response(content='ok', media_type="text/plain")
-    return Response(content='ok', media_type="text/plain")
+            elif data['type'] == 'message_event':
+                message_info = data['object']
+                user_id = message_info.get('peer_id', 'Не указано')
+                payload = data['object']['payload']
+                payload_user_act = payload.get('command', 'Не указано')
+                user_act = await change_act_after_callback(user_id, payload_user_act)
+
+                asyncio.create_task(user_act_handler(user_id, user_act))
+                return Response(content='ok', media_type="text/plain")
+
+        return Response(content='ok', media_type="text/plain")
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return None
 
 
 if __name__ == '__main__':
